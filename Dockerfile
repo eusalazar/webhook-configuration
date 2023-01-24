af942d543fb53f956669de8c296059bc997f1de6
Eugenia Salazar Acuña <90300459+eusalazar@users.noreply.github.com>
2023-01-24 20:57:56 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ruby:latest
+
+WORKDIR /usr/src/app
+COPY src/ .
+RUN gem install sinatra
+RUN gem install json
+RUN gem install puma
+
+EXPOSE 3600
+
+CMD ruby src/server.rb -p $VIRTUAL_PORT
---
+++ src/server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'json'
+
+post '/payload' do
+  push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+
+  workdir = "/root/porfolio-dev"
+  Dir.chdir(workdir) do
+    puts "Tirando docker compose"
+
+    down_compose = "dc down"
+    system down_compose
+    puts "Rebaseando git"
+    system "git pull --rebase"
+    puts "Instalndo npm"
+    system "npm run build"
+
+    puts "Corriendo el build de docker."
+    system "dc up --build"
+    puts "Finished"
+  end
+
+end
